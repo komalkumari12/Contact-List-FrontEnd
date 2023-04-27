9e979ec0e2082c73f0285c1849052f0de163b6c1
komal <kk6612465@gmail.com>
2023-04-28 02:05:26 +0530

    Add functionality Done

---
+++ src/components/form.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+export const Form = () => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [contacts, setContacts] = useState([]);
+
+  function addToContact(event) {
+    event.preventDefault();
+    const newContact = {
+      id: contacts.length + 1,
+      name,
+      number,
+    };
+
+    setContacts([...contacts, newContact]);
+    setName("");
+    setNumber("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={addToContact}>
+        <label>Add Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <label>Add Number</label>
+        <input
+          type="tel"
+          value={number}
+          onChange={(event) => {
+            setNumber(event.target.value);
+          }}
+        ></input>
+
+        <button type="submit">Add to Contact</button>
+      </form>
+
+      <ul>
+        {contacts.map((iterator, index) => {
+          return (
+            <li key={iterator.id}>
+              <p>{iterator.name}</p>
+              <p>{iterator.number}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
